576ac5beeb14948133ced1ecd062a27e204f9ef2
perforb <dev.perfumed.garden@gmail.com>
2023-10-24 23:31:09 +0900

    feat: added Selector component

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,9 +1,11 @@
 import {Title} from "../components/Title";
+import {Selector} from "../components/Selector";
 
 const TopPage = () => {
   return (
     <>
       <Title/>
+      <Selector/>
     </>
   );
 };
