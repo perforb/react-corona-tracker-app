4190933ed6f7c1993635ea87b37711b60e564adb
perforb <dev.perfumed.garden@gmail.com>
2023-10-25 07:56:59 +0900

    feat: add state

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,9 +1,11 @@
 import countriesJson from "../countries.json";
+import {useState} from "react";
 
 const Selector = () => {
+  const [country, setCountry] = useState("");
   return (
     <div>
-      <select>
+      <select onChange={(e) => setCountry(e.target.value)}>
         {countriesJson.map((country, index) =>
           <option key={index} value={country.Slug}>{country.Country}</option>
         )}
