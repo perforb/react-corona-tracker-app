c41100de6e01121998bf31a82b2c3c822d18c00b
perforb <dev.perfumed.garden@gmail.com>
2023-10-26 07:43:06 +0900

    feat: completed Selector component

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -3,13 +3,20 @@ import {useState} from "react";
 
 const Selector = () => {
   const [country, setCountry] = useState("");
+  const getCountryData = () => {
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
   return (
     <div>
       <select onChange={(e) => setCountry(e.target.value)}>
+        <option>Select A Country</option>
         {countriesJson.map((country, index) =>
           <option key={index} value={country.Slug}>{country.Country}</option>
         )}
       </select>
+      <button onClick={getCountryData}>Get Data</button>
     </div>
   );
 };
